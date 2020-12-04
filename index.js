9e5973547a51d86f46d6ce60a4e9974dab2c1f38
radcleong94 <radcleong@gmail.com>
2020-12-04 21:47:27 +0800

    nightmode update

--- index.js
+++ index.js
@@ -17,3 +17,47 @@ var scene = new ScrollMagic.Scene({
     });
 
   });
+
+// Night mode function//
+  let myFunction = false;
+  let setButton = document.querySelector('.setBtn');
+
+ function nightMode(){
+
+    if(setButton.textContent === "Night Mode"){
+        setButton.textContent = "Bright Mode";
+        document.body.style.background = "#000000cf";
+        document.body.style.opacity = .8;
+        setButton.style.color = "black";
+        setButton.style.background = "white";
+        setButton.style.border = "1px solid white";
+
+        let x = document.querySelectorAll('h3');
+        for(var i=0;i < x.length;i++){
+          x[i].style.color = "white";
+        };
+        
+        let y = document.querySelectorAll('.nightmode');
+        for(var j=0;j < y.length; j++){
+          y[j].style.color = 'white';
+        }
+  }else{
+    setButton.textContent = "Night Mode";
+    setButton.style.color = "white";
+    setButton.style.background = "rgb(53, 53, 53)";
+    setButton.style.border = "1px solid rgb(53, 53, 53)";
+
+    document.body.style.background = "rgb(238, 249, 252)";
+        document.body.style.opacity = 1;
+        
+        let x = document.querySelectorAll('h3');
+        for(var i=0;i < x.length;i++){
+          x[i].style.color = "black";
+        };
+        
+        let y = document.querySelectorAll('.nightmode');
+        for(var j=0;j < y.length; j++){
+          y[j].style.color = 'black';
+        }
+  }
+  }
